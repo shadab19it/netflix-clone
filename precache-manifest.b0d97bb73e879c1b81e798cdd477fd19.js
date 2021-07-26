9d61b6d7fb4aff1b6c772b4d9d167580409ad272
Shadab <shadab19it@gmail.com>
2021-07-26 10:28:22 +0530

    Updates

---
+++ precache-manifest.b0d97bb73e879c1b81e798cdd477fd19.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ebd4fc49adc78ab3b195d112cc752a52",
+    "url": "/netflix-clone/index.html"
+  },
+  {
+    "revision": "898c2e4314ac28f2a590",
+    "url": "/netflix-clone/static/css/main.187e3ebf.chunk.css"
+  },
+  {
+    "revision": "5a50bfee63a89dd6e98f",
+    "url": "/netflix-clone/static/js/2.03502767.chunk.js"
+  },
+  {
+    "revision": "894f61387b383e6e2e0543fb120b4656",
+    "url": "/netflix-clone/static/js/2.03502767.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "898c2e4314ac28f2a590",
+    "url": "/netflix-clone/static/js/main.52bc751f.chunk.js"
+  },
+  {
+    "revision": "42ccba1c2e183f5632cb",
+    "url": "/netflix-clone/static/js/runtime-main.baddefd0.js"
+  },
+  {
+    "revision": "e5692be8ee2f6b85f84cb0a949c88fa4",
+    "url": "/netflix-clone/static/media/netflix.e5692be8.png"
+  }
+]);
